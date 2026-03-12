f5878e2e005ed5320e508af07fa2b121a23ab4db
lucasmachadocamargo-code <lucas.machado.camargo@escola.pr.gov.br>
2026-03-12 14:43:02 +0000

    nao e numero

---
+++ type-numero.js
@@ -0,0 +1,21 @@
+// tipo number
+const meunumero = 3;
+const primeiroNumero =1;
+const segundoNumero = 2;
+const operaçãoMatematica = primeiroNumero + segundoNumero;
+
+console.log(operaçãoMatematica)
+
+//ponto flutuante 
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+console.log (novaOperacao);
+
+
+//NaN -> not a nunber (nao é um número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero);
